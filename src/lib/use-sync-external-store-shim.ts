a541b312075331560844214ff82f1c7f8d5377c6
Maximilian PINDER-WHITE <mpinderwhite@proton.me>
2026-03-06 14:53:19 +0100

    remove workaround

--- src/lib/use-sync-external-store-shim.ts
+++
@@ -1,6 +0,0 @@
-// React 19 has useSyncExternalStore built-in
-// This shim redirects packages that import from use-sync-external-store/shim
-import { useSyncExternalStore } from 'react'
-
-export { useSyncExternalStore }
-export default useSyncExternalStore
